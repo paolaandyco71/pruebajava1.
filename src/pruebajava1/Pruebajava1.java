3b906eb3a42a1e168002aaa0c536fad98357822e
paolaandyco71 <paolaandyco71@gmail.com>
2017-10-30 21:47:43 -0300

    se calcula el area del rectangulo

--- src/pruebajava1/Pruebajava1.java
+++ src/pruebajava1/Pruebajava1.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package pruebajava1;
-
 
 /******************************************************************************
  *Paola Bravo Cardenas
@@ -12,33 +10,107 @@ package pruebajava1;
  * Fecha:30 de octubre 2017
  * Programa: Prueba java 1.
  * Responsabilidad: Calcular el area del rectangulo
- */
-public class Pruebajava1 
+ ******************************************************************************
+*/
+public class pruebajava1.
 {
-    public static void main (String[]arg)
+     publicstatic void main(String[] args)
+{
+     // TODO code application logic here
+
+       int area_rectangulo=0;
+          int base = 5;
+       
+        //Area del Rectangulo
+
+       int areaRectangulo = 0;
+
+        //Aqui ingresamos la base
+
+       int base = 5;
+       
+        //Aqui ingresamos la altura
+
+       int altura = 4;
+
+       area_rectangulo = base altura;
+
+       System.out.println("El are de un rectangulo de base"+base+" y altura "+altura+") es igual a "area_rectangulo);
+       
+       //Aqui realizamos el calculo de base por altura
+       area rectacgulo=base altura;
+       System.out.println("El area de un rectangulo ");
+
+}
+
+
+
+}
+}
+
+
+
+{
+{
+
+   
+
+
+
+
+}
+}
+
+        
+       
+
+{
+   
+            
+            
+    
     
     {
-        int 
-    }
             
             
-            
-    
     
 }
+    
+  
+  
+ 
+   
         
         
-{   
+    
+
+
+
+    
+
+   
+        
+
+    
+    
+       
+        
+    
+            
+            
+            
+    
+    
+
+        
+        
+  
      
 
     
-  
-
-    /**
+     /**
      *
      */
    
         // TODO code application logic here
-    
-    
-}
+   
